35c63b44dda74b65cd979e5e88a9480691c3ba9a
Muhammad Imtiaz <muhammadimtiazhossen2005@gmail.com>
2023-12-30 00:20:48 +0600

    oyon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
     spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      769: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -10,3 +32,27 @@ var swiper = new Swiper('.swiper-container', {
         clickable: false,
     },
 });
+
+// const swiper = new Swiper('.swiper-container', {
+//     // Default parameters
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     // Responsive breakpoints
+//     breakpoints: {
+//       // when window width is >= 320px
+//       320: {
+//         slidesPerView: 2,
+//         spaceBetween: 20
+//       },
+//       // when window width is >= 480px
+//       480: {
+//         slidesPerView: 3,
+//         spaceBetween: 30
+//       },
+//       // when window width is >= 640px
+//       640: {
+//         slidesPerView: 4,
+//         spaceBetween: 40
+//       }
+//     }
+//   })
